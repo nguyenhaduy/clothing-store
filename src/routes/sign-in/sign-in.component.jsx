0f33d60f5a4305dee58926a5f4532a624348fe28
Drake Nguyen <drakehanguyen@gmail.com>
2024-02-27 08:41:17 -0600

    created sign up and sign in

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.components";
+
 const SignIn = () => {
+  useEffect( async () => {
+    const response = await getRedirectResult(auth);
+    if (response) {
+      const userDocRef = await createUserDocumentFromAuth(response.user);
+    }
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -13,6 +27,10 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>SignIn with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        SignIn with Google Redirect
+      </button>
+      <SignUpForm />
     </div>
   );
 };
